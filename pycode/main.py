99dce9c8528dadeb43f62559fb81cbd5cf3d2917
Tobias J <tobias.jaksties@outlook.com>
2022-02-17 14:46:46 +0100

    added gamemode py file and made switch statement in main for test

--- pycode/main.py
+++ pycode/main.py
@@ -5,14 +5,32 @@ import dice
 def main():
     
     menu()
+
+    x = input(str("select option "))
+    match x:
+        case '1':
+            print("test case 1")
+        case '2':
+            print("test case 2")
+            
+                
     
     test_die = dice.Dice()
     test_num = test_die.roll_the_dice()
     print(test_num)
 
 def menu():
-    print("""Welcome to the game! 
-    below you will find your options! """)
+    print(""" 
+    Welcome to the game! 
+    below you will find your options!
+    
+    1) create character
+    2) view highscores
+    3) play against computer
+    4) play 1v1 
+    5) watch computer play against itself """)
+
+    
 
 
 
