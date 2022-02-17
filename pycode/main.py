23d88b0140573c40149efd14199c1d3b4d0fc13b
Tobias J <tobias.jaksties@outlook.com>
2022-02-17 14:55:44 +0100

    separeted menu and options into two separate def's

--- pycode/main.py
+++ pycode/main.py
@@ -6,12 +6,19 @@ def main():
     
     menu()
 
+    options()
     x = input(str("select option "))
     match x:
         case '1':
-            print("test case 1")
+            print("här kör vi gamemode 1")
         case '2':
-            print("test case 2")
+            print("gamemode 2")
+        case '3':
+            print("gamemode 3")
+        case '4':
+            print("skapa karaktär här ? ")
+        case '5':
+            print("kolla highscore här ? ")
             
                 
     
@@ -22,13 +29,20 @@ def main():
 def menu():
     print(""" 
     Welcome to the game! 
-    below you will find your options!
-    
-    1) create character
-    2) view highscores
-    3) play against computer
-    4) play 1v1 
-    5) watch computer play against itself """)
+    This is the game pig!
+    The rules are : you will throw the dice and the amount will be added to your total.
+    If the dice stops on 1, you total amount for the round will be set to 0
+    first to 100 points win!
+    no cheating! (except with command "i am a cheat")
+    """)
+
+def options():
+    print("""This is your options:
+    1) play game vs computer
+    2)play game vs other player
+    3) watch computer play itself
+    4) create character
+    5) view local highscores""")
 
     
 
